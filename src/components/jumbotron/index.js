737a97234e2bb5aa9783ae936f782a47f283e63c
Yeff Espinoza <espinozayeff.git@gmail.com>
2020-10-18 18:59:03 -0500

    Add jumbotron styled component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Inner } from './styles/Jumbotron';
+
+export default () => (
+	<Inner>
+		Hello!
+	</Inner>
+);
---
+++ src/components/jumbotron/styles/Jumbotron.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+
+export const Inner = styled.div`
+	max-width: 1100px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`
